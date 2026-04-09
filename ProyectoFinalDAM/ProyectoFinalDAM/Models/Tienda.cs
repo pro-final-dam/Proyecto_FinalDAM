0d1da1b723e853fd4f0eb290cecdbb3ee9898cd2
martinbb8 <ciclismoacuatico@gmail.com>
2026-04-09 10:17:49 +0200

    Finalización Dashboard y Memoria Técnica

---
+++ ProyectoFinalDAM/ProyectoFinalDAM/Models/Tienda.cs
@@ -0,0 +1,16 @@
+﻿namespace ProyectoFinalDAM.Models
+{
+    public class Tienda
+    {
+        public string? codigo_tienda { get; set; }
+        public string? nombre_tienda { get; set; }
+        public string? ciudad { get; set; }
+        public string? fuente { get; set; }
+
+        // Mapeamos el objeto de meses
+        public Dictionary<string, double>? ventas_mes { get; set; }
+
+        // Propiedad calculada: Sumamos los valores del diccionario para obtener el total
+        public double ventas_anuales => ventas_mes?.Values.Sum() ?? 0;
+    }
+}
